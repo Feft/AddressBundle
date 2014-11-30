8383afa9e8f3a37e0d90ab04bf367d45e6290ed2
Piotr Pikuła <piotrpikula@gmail.com>
2014-11-30 12:38:02 +0100

    class Street

---
+++ Tests/StreetTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Feft\AddressBundle\Tests;
+
+
+use Feft\AddressBundle\Entity\Street;
+
+class StreetTest extends \PHPUnit_Framework_TestCase {
+    public function testCreation()
+    {
+        $this->assertInstanceOf("Feft\AddressBundle\Entity\Street", new Street());
+    }
+
+    public function testSettersAndGetters()
+    {
+        $street = new Street();
+        $name = "Myśliwska";
+        $street->setName($name);
+        $this->assertSame($name,$street->getName());
+        $this->assertNull($street->getId());
+    }
+}
+ 
