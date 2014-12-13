50e3f03456de6302a985aa4467a5d109a47b9be3
Piotr Pikuła <piotrpikula@gmail.com>
2014-12-13 19:13:11 +0100

    polish address format example

--- Model/AddressFormatter/CountryFormatters/PL.php
+++ Model/AddressFormatter/CountryFormatters/PL.php
@@ -5,13 +5,26 @@ namespace Feft\AddressBundle\Model\AddressFormatter\CountryFormatters;
 class PL extends DefaultFormatter {
 
     /**
-     * Polish version.
+     * Polish version, for example:
+     *  Poste Polonaise
+        Direction générale - example
+        Bureau du trafic international
+        Pl. Malachowskiego 2
+        00-940 WARSZAWA
+        POLAND
      * @link http://www.bitboost.com/ref/international-address-formats/poland/
      * @inheritdoc
      */
     public function getFormattedAddress($options = array())
     {
-        return "adres po polsku";
+        return "
+            Poste Polonaise
+            Direction générale - example
+            Bureau du trafic international
+            Pl. Malachowskiego 2
+            00-940 WARSZAWA
+            POLAND
+        ";
     }
 
 } 
