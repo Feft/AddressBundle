a085dcbae97030d5267d8dca5b1facf66b35e207
Piotr Pikuła <piotrpikula@gmail.com>
2014-12-19 07:45:51 +0100

    Update FeftAddressBundle.php

--- FeftAddressBundle.php
+++ FeftAddressBundle.php
@@ -4,6 +4,10 @@ namespace Feft\AddressBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Bundle configuration class.
+ * @inheritdoc 
+ */
 class FeftAddressBundle extends Bundle
 {
 }
