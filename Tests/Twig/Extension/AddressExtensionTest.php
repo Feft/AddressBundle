c293dc20fe90cb3412f5364063d4865076e13913
Piotr Pikuła <piotrpikula@gmail.com>
2014-12-28 14:46:22 +0100

    Twig extension unit test

---
+++ Tests/Twig/Extension/AddressExtensionTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Feft\AddressBundle\Tests;
+
+use Feft\AddressBundle\Twig\Extension\AddressExtension;
+
+class AddressExtensionTest extends \PHPUnit_Framework_TestCase {
+    public function testGetFunctions()
+    {
+        $extension = new AddressExtension();
+        $functionsArray = $extension->getFunctions();
+
+        $this->assertCount(2,$functionsArray);
+
+        foreach($functionsArray as $a) {
+            $this->assertInstanceOf("\Twig_Function_Method",$a);
+        }
+    }
+
+    public function testGetName()
+    {
+        $extension = new AddressExtension();
+        $this->assertSame("feft_address_extension",$extension->getName());
+    }
+}
